f5b364c0c6a7e1bb79cff2858d4aa7fbcc679754
Tommy Saechao <tfsaechao@ucdavis.edu>
2021-03-03 00:15:47 -0800

    Added Toolbar UI and refactored some code in the UI

--- src/org/ecs160/a2/UI.java
+++ src/org/ecs160/a2/UI.java
@@ -1,4 +1,27 @@
 package org.ecs160.a2;
 
+import org.ecs160.a2.UI_PAGES.MainPageUI;
+
 public class UI {
+    MainPageUI mainPageUI = new MainPageUI();
+
+    public void initUI(Object context)
+    {
+        mainPageUI.initUI(context);
+    }
+
+    public void startUI()
+    {
+        mainPageUI.startUI();
+    }
+
+    public void stopUI()
+    {
+        mainPageUI.stopUI();
+    }
+
+    public void destroyUI()
+    {
+        mainPageUI.destroyUI();
+    }
 }
