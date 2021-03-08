0e4a1e62d13842502c92ba3712c0a48daba5c729
Tommy Saechao <tfsaechao@ucdavis.edu>
2021-03-07 21:04:30 -0800

    Implemented Summary Page UI template along with back and edit navigation functionality

--- src/org/ecs160/a2/UI_PAGES/SummaryPageUI.java
+++ src/org/ecs160/a2/UI_PAGES/SummaryPageUI.java
@@ -11,27 +11,57 @@ import static com.codename1.ui.CN.CENTER;
 public class SummaryPageUI {
 
     Form skeleton;
+    Button backButton = new Button("Back");
+    Label statusLabel = new Label("Status");
+    Button startButton = new Button("Start");
+    Button stopButton = new Button("Stop");
+    Label nameLabel = new Label("Name");
+    Label descriptionLabel = new Label("Description");
+    Label sizeLabel = new Label("Size");
+    Label taskDataLabel = new Label("Task Data");
+    Label minTimeLabel = new Label("Min Time: ");
+    Label maxTimeLabel = new Label("Max Time: ");
+    Label avgTimeLabel = new Label("Avg Time: ");
+    Button deleteButton = new Button("Delete");
+    Button editButton = new Button("Edit");
+
+
     public static SummaryPageUI infoPage = new SummaryPageUI();
 
     public void loadSummaryPageUI(String taskName) {
-        skeleton = new Form("Info: " + taskName, new BorderLayout());
-
-        TableLayout tl;
-        int         spanButton = 2;
-        if(Display.getInstance().isTablet()) {
-            tl = new TableLayout(7, 2);
-        } else {
-            tl = new TableLayout(14, 1);
-            spanButton = 1;
+        if (skeleton != null)
+        {
+            //TODO: Implement function to load task info and data (requires
+            // backend to be finished)
+            skeleton.show();
+            return;
         }
+        initSummaryPage(taskName);
+        skeleton.show();
+    }
+
+    public void initSummaryPage(String taskName)
+    {
+        skeleton = new Form("Info: " + taskName, new BorderLayout());
+        TableLayout tl = new TableLayout(11, 2);
         tl.setGrowHorizontally(true);
         skeleton.setLayout(tl);
-
-        Button editBtn = new Button("Edit");
-        editBtn.addActionListener((e) -> EditPageUI.editPage.loadEditPageUI(taskName));
-
-        skeleton.add(editBtn);
-
-        skeleton.show();
+        editButton.addActionListener((e) -> EditPageUI.editPage.loadEditPageUI(taskName));
+        backButton.addActionListener((e) -> MainPageUI.mainPage.loadMainPageUI());
+        skeleton.
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), backButton).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), statusLabel).
+        add(tl.createConstraint().horizontalSpan(1).horizontalAlign(Component.LEFT), startButton).
+        add(tl.createConstraint().horizontalSpan(1).horizontalAlign(Component.LEFT), stopButton).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), nameLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), descriptionLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), sizeLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), new Label(" ")).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), taskDataLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), minTimeLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), maxTimeLabel).
+        add(tl.createConstraint().horizontalSpan(2).horizontalAlign(Component.LEFT), avgTimeLabel).
+        add(tl.createConstraint().horizontalSpan(1).horizontalAlign(Component.LEFT), deleteButton).
+        add(tl.createConstraint().horizontalSpan(1).horizontalAlign(Component.LEFT), editButton);
     }
 }
