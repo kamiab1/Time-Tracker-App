b7b0fc5341e6253a47fc7e09e85d25c27ff01e36
kamiab1 <kamiab.ayani@gmail.com>
2021-03-07 21:59:32 -1000

    Added more abstraction for UI
    bundled up each UI element into a function
    1- increase reusability 
    2- reduced dry code
    3- much more readable

--- src/org/ecs160/a2/UI_PAGES/MainPageUI.java
+++ src/org/ecs160/a2/UI_PAGES/MainPageUI.java
@@ -30,51 +30,76 @@ public class MainPageUI
 
     public void startUI() {
         // NOT SURE NEEDS MORE TESTS
-//        if(skeleton != null){
-//            skeleton.show();
+//        if(scaffold != null){
+//            scaffold.show();
 //            return;
 //        }
 
+        List<Task> taskList = getAllTasks();
         scaffold = new Form("Task List", new BorderLayout());
+        toolBarSetup();
+        Container taskListView = createListView();
 
-        Command createTask = new Command("Create Task") {
-            public void actionPerformed(ActionEvent e) {
-                CreateTaskPageUI.createTaskPage.startUI();
-            }
-        };
-
-        scaffold.getToolbar().addCommandToRightBar(createTask);
-        Container list = new Container(BoxLayout.y());
-        list.setScrollableY(true);
-        scaffold.add(CENTER, list);
-
-        List<Task> taskList = store.getAllTasks();
-        final List<Container> contList = new ArrayList<Container>();
-
+        // show the list of all tasks
         taskList.forEach( eachTask -> {
-            System.out.print("HERE \n");
-            // build container
-            Container container = new Container(BoxLayout.x());
-            container.setWidth(scaffold.getWidth());
-            container.setY(scaffold.getHeight());
-            // build button
-            MultiButton taskButton = new MultiButton(eachTask.name);
-            taskButton.setWidth(scaffold.getWidth());
-            taskButton.addActionListener((e)-> goToInfoPage(eachTask));
-            // add to list
-            container.addComponent(taskButton);
-            contList.add(container);
-            list.addComponent(container);
+            Container taskContainer = makeTaskContainer();
+            MultiButton taskButton = makeTaskBtn(eachTask);
+
+            taskContainer.addComponent(taskButton);
+            taskListView.addComponent(taskContainer);
         });
 
-
         scaffold.show();
+    }
+
+
+    /*************** General functions ****************/
+
+    private List<Task> getAllTasks() {
+        return store.getAllTasks();
     }
 
     private void goToInfoPage(Task task) {
         System.out.print("pressed :" + task.name);
         InfoPageUI.infoPage.startUI(task);
     }
+
+
+    /*************** UI functions ****************/
+
+    private void toolBarSetup() {
+        Command createTaskCommand = new Command("Create Task") {
+            public void actionPerformed(ActionEvent e) {
+                CreateTaskPageUI.createTaskPage.startUI();
+            }
+        };
+        scaffold.getToolbar().addCommandToRightBar(createTaskCommand);
+    }
+
+    private Container createListView() {
+        Container taskListView = new Container(BoxLayout.y());
+        taskListView.setScrollableY(true);
+        scaffold.add(CENTER, taskListView);
+        return  taskListView;
+    }
+
+    private MultiButton makeTaskBtn(Task eachTask) {
+        MultiButton taskButton = new MultiButton(eachTask.name);
+        taskButton.setWidth(scaffold.getWidth());
+        taskButton.addActionListener((e)-> goToInfoPage(eachTask));
+        return taskButton;
+    }
+
+    private Container makeTaskContainer() {
+        Container container = new Container(BoxLayout.x());
+        container.setWidth(scaffold.getWidth());
+        container.setY(scaffold.getHeight());
+        return container;
+    }
+
+
+
+    /*************** Clean up ****************/
 
     public void stopUI() {
         scaffold = getCurrentForm();
