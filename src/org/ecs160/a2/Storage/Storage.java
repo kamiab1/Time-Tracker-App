f564b3771c4ce2987e4cbe9ceb19153e514f7672
kamiab1 <kamiab.ayani@gmail.com>
2021-03-08 10:02:58 -1000

    Decoupled LocalStorage implementation from Storage
    1- Abstracted the actual implementation of how the data is saved
    2- now we can easily change the way we save data with no breaking changes
    3- easily add could storage in the future

--- src/org/ecs160/a2/Storage/Storage.java
+++ src/org/ecs160/a2/Storage/Storage.java
@@ -1,14 +1,49 @@
 package org.ecs160.a2.Storage;
 
+import org.ecs160.a2.model.Task;
+import java.util.List;
+
+
+/** Flexibility to add CloudStorage in the future **/
+
 public class Storage {
+    private final LocalStorage localStorage = new LocalStorage();
+    //* CloudStorage => for the Future if needed
     private static Storage SingletonInstance;
 
     public static Storage instanse() {
         if(SingletonInstance == null) {
             SingletonInstance = new Storage();
         }
-
         return SingletonInstance;
     }
 
+
+    /****************** public *******************/
+
+
+    /******** GET ********/
+
+    public Task getTask(String taskName) {
+        return localStorage.getTask(taskName);
+    }
+
+    public List<Task> getAllTasks () {
+        return localStorage.getAllTasks();
+    }
+
+
+    /******** SET ********/
+
+    public void addTask(Task task) {
+        localStorage.addTask(task);
+    }
+
+    public void editTask(Task task) {
+
+    }
+
+    public void deleteTask(Task task) {
+        localStorage.deleteTask(task);
+    }
 }
